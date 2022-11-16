17c7f5b4395423569c89830f98c2a1315b356e2f
farells <f.sujanto@gmail.com>
2022-11-17 02:38:58 +0700

    Improve: Add selection capabilities

--- src/components/buttons/HomeTimeDropdown/index.tsx
+++ src/components/buttons/HomeTimeDropdown/index.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 
-const HomeTimeDropdown = () => {
-return(
-    <select className="p-1 text-gray-900 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-    >
-        <option selected>24 Jam</option>
-        <option>1 MGG</option>
-        <option>1 BLN</option>
-        <option>1 THN</option>
-        
-    </select>
-);
+interface HomeTimeDropdownProps {
+    dayFilter: string;
+    onDayFilterChange: (value: string) => void;
+}
 
-
+const HomeTimeDropdown = ({ onDayFilterChange, dayFilter }: HomeTimeDropdownProps) => {
+    return (
+        <select
+            className='p-1 text-gray-900 bg-white border rounded-md shadow-sm outline-none'
+            value={dayFilter}
+            onChange={(e) => {
+                onDayFilterChange(e.target.value);
+            }} >
+            <option selected value='day'>24 Jam</option>
+            <option value='week'>1 MGG</option>
+            <option value='month'>1 BLN</option>
+            <option value='year'>1 THN</option>
+        </select>
+    );
 }
 
 export default React.memo(HomeTimeDropdown);

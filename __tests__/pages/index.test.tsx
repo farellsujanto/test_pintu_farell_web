8dfee98f4039d3c7603532c7bbea39d50939e045
farells <f.sujanto@gmail.com>
2022-11-17 17:35:00 +0700

    Feat: Add dummy data rendering test

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -1,7 +1,21 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-import { render, screen } from '@testing-library/react'
-import { defaultQueryClient } from '../src/configs/queryClient';
-import Home from '../src/pages';
+import { render, screen } from '@testing-library/react';
+import { defaultQueryClient } from '../../src/configs/queryClient';
+import Home from '../../src/pages';
+import MarketListTableDesktop from '../../src/components/tables/MarketListTable/desktop';
+import MarketListTableMobile from '../../src/components/tables/MarketListTable/mobile';
+
+const dummyMarketChangesData = [{
+    name: 'Ethereum',
+    currencySymbol: 'eth',
+    logo: '',
+    color: '#ffffff',
+    day: '10000',
+    week: '20000',
+    month: '30000',
+    year: '40000',
+    latestPrice: '50000',
+}];
 
 describe('Home', () => {
     it('Renders correctly without crashing', () => {
@@ -16,5 +30,47 @@ describe('Home', () => {
         });
 
         expect(heading).toBeInTheDocument();
+    });
+
+    it('Check if Desktop table render data correctly (simple only)', async () => {
+        const wrapper = render(
+            <MarketListTableDesktop
+                marketChangesListData={dummyMarketChangesData} />
+        );
+
+        const table = wrapper.getByRole('table');
+        const td = table.children[1];
+
+        const isNameCorrect = td.innerHTML.includes(dummyMarketChangesData[0].name);
+
+        expect(td).toBeInTheDocument();
+        expect(isNameCorrect).toBeTruthy();
+    });
+
+    it('Check if Mobile table render data correctly (simple only)', async () => {
+        const wrapper = render(
+            <MarketListTableMobile
+                marketChangesListData={dummyMarketChangesData} />
+        );
+
+        const table = wrapper.getByRole('table');
+        const td = table.children[1];
+
+        const isNameCorrect = td.innerHTML.includes(dummyMarketChangesData[0].name);
+
+        expect(td).toBeInTheDocument();
+        expect(isNameCorrect).toBeTruthy();
+    });
+
+    it('Check if Mobile table render data correctly with 24H filter (simple only)', async () => {
+        const wrapper = render(
+            <MarketListTableMobile
+                marketChangesListData={dummyMarketChangesData} />
+        );
+
+        expect(wrapper.getByText('24 Jam')).toBeInTheDocument();
+
+        const isDayFilterCorrect = wrapper.getByRole('table').children[1].innerHTML.includes(dummyMarketChangesData[0].day);
+        expect(isDayFilterCorrect).toBeTruthy();
     });
 });

789707be0a04bec8620451a9133236b574609a56
farells <f.sujanto@gmail.com>
2022-11-17 16:20:29 +0700

    Feat: Setup jest & sample test case

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,20 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { render, screen } from '@testing-library/react'
+import { defaultQueryClient } from '../src/configs/queryClient';
+import Home from '../src/pages'
+
+describe('Home', () => {
+    it('Renders correctly without crashing', () => {
+        render(
+            <QueryClientProvider client={defaultQueryClient}>
+                <Home />
+            </QueryClientProvider>
+        );
+
+        const heading = screen.getByRole('heading', {
+            name: 'Harga Crypto dalam Rupiah Hari Ini',
+        });
+
+        expect(heading).toBeInTheDocument();
+    })
+})
